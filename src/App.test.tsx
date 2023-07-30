ae2ed30043f2e671d4d5ecc2fa061a6bc505071c
Aritra <aet419@gmail.com>
2023-07-30 23:19:06 +0530

    infinity scroll

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,18 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import React from "react";
+import { render, screen, waitFor } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe("Test for Count", () => {
+  beforeEach(() => {
+    render(<App />);
+  });
+  test("count element showing", () => {
+    expect(screen.getByTestId("count")).toBeInTheDocument();
+  });
+  test("count to be 10", async () => {
+    await waitFor(()=>{
+      expect(screen.getByTestId("count")).toHaveTextContent("count: 10");
+    })
+    
+  });
 });
--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,79 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useRef, useState } from "react";
+// import "./App.css";
 
+interface Item {
+  id: number;
+  first_name: string;
+  last_name: string;
+  email: string;
+  gender: string;
+  ip_address: string;
+}
 function App() {
+  const [data, setData] = useState<Item[]>([]);
+  const ref = useRef<HTMLDivElement>(null);
+  const pageRef = useRef(1);
+  const abortController = useRef<AbortController>();
+  const COUNT = 1000
+
+  function fetchItem() {
+    abortController.current = new AbortController();
+    const signal = abortController.current.signal;
+    // http://localhost:3011/items
+    fetch(`https://jsonplaceholder.typicode.com/comments?_page=${pageRef.current}`, { signal })
+      .then((resp) => resp.json())
+      .then((json) => setData((prev: Item[]) => [...prev, ...json]))
+      .catch(console.error);
+  }
+
+  function partialPull(ev: any) {  
+    const element = ref.current;
+    if (element !== null) {
+      if (data.length === COUNT) {
+        element.removeEventListener("scroll", partialPull)
+      }
+      const maxScroll = element?.scrollHeight - element?.clientHeight;
+      const percent = (element?.scrollTop * 100) / maxScroll;
+      if (percent === 100) {
+        pageRef.current = pageRef.current + 1;
+        fetchItem();
+        // console.log(`scrollTop:`, percent, "page", pageRef.current);
+      }
+    }
+  }
+  useEffect(() => {
+    fetchItem();
+    const element = ref.current;
+    element?.addEventListener("scroll", partialPull);
+
+    return () => {
+      element?.removeEventListener("scroll", partialPull);
+      setData([]);
+      abortController.current?.abort();
+    };
+  }, []);
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div
+        ref={ref}
+        id="App"
+        style={{
+          maxHeight: 210,
+          width: "fit-content",
+          overflow: "auto",
+          background: "yellow",
+        }}
+      >
+        <ul>
+          {data.map((item, index) => (
+            <li key={index}>
+              {item?.id} {item.email}
+            </li>
+          ))}
+        </ul>
+      </div>
+      <p data-testid="count">count: {data?.length}</p>
     </div>
   );
 }
